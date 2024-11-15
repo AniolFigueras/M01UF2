3d92fab7c1194458cf0041395bf953a8740323e6
Aniol <aniol.figueras@enti.cat>
2024-11-15 10:04:02 +0100

    Añadiendo el File Name y la comprobacion

--- client.sh
+++ client.sh
@@ -11,10 +11,29 @@ echo "DMAM" | nc 127.0.0.1 $PORT
 
 DATA=`nc -l $PORT`
 
-# SI DATA ES DIFERENTE A OK HEADER, MENSAJE
-#DE ERROR Y EXIT 1
-if [ "$DATA " != "OK_HEADER" ]
+echo "3. COMPROBANDO HEADER"
+
+# SI DATA ES DIFERENTE A OK_HEADER, MENSAJE
+# DE ERROR Y EXIT 1
+if [ "$DATA" != "OK_HEADER" ]
 then
 	echo "ERROR 1: el header se envio incorrectamente"
 	exit 1
 fi
+
+echo "4. ENVIADO EL FILE_NAME"
+
+FILE_NAME="dragon.txt"
+
+echo "FILE_NAME $FILE_NAME" | nc localhost $PORT
+
+DATA=`nc -l $PORT`
+
+echo "7. RECIBIENDO COMPROBACION FILE_NAME"
+
+if [ "$DATA" != "OK_FILE_NAME"]
+then
+	echo "ERROR 2: El filename se envio incorrectamente"
+	exit 2
+fi
+
--- server.sh
+++ server.sh
@@ -19,3 +19,20 @@ fi
 echo "2. CHECK OK - Enviando OK_HEADER"
 echo "OK_HEADER" | nc localhost 2022
 DATA=`nc -l $PORT`
+
+echo "5. COMPROBANDO PREFIJO"
+
+PREFIX=`echo "$DATA" | cut -d ' ' -f 1`
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "ERROR 2: Prefijo incorrecto"
+	echo "KO_FILE_NAME" | nc localhost $PORT
+	exit 2
+fi
+
+echo "6. ENVIANDO OK_FILE_NAME"
+
+echo "OK_FILE_NAME" | nc localhost $PORT
+
+DATA=`nc -l $PORT`
